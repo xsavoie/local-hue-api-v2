20e79a1327cc5105fb4a97ddc88dbb6a2759b48f
Xavier <xaviersavoie@gmail.com>
2022-02-01 14:33:20 -0500

    implemented get grouped_light route

--- backend/routes/grouped_light.js
+++ backend/routes/grouped_light.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const grouped_light = express.Router();
+const axios  = require('axios');
+require('dotenv').config();
 
-grouped_light.get('/', function(req, res) {
-  res.send('test -- grouped_light');
+const bridge = process.env.HUE_BRIDGE;
+const username = process.env.HUE_USERNAME;
+
+grouped_light.get('/', async function(req, res) {
+  try {
+    const apiResponse = await axios.get(
+      `https://${bridge}/clip/v2/resource/grouped_light`,
+      {
+        headers: {
+          "hue-application-key": username,
+        },
+      }
+    );
+    console.log(apiResponse.data.data)
+    res.json(apiResponse.data.data)
+  } catch (error) {
+    console.log(error);
+  }
+  // res.send('test -- grouped_light');
 });
 
 module.exports = grouped_light;
--- backend/routes/light.js
+++ backend/routes/light.js
@@ -1,23 +1,18 @@
-const axios  = require('axios');
 const express = require('express');
 const light = express.Router();
+const axios  = require('axios');
 require('dotenv').config();
-
 
 const bridge = process.env.HUE_BRIDGE;
 const username = process.env.HUE_USERNAME;
 
 light.get('/', async function(req, res) {
-  // console.log("bridge", bridge)
-  // console.log("username", username)
   try {
     const apiResponse = await axios.get(
       `https://${bridge}/clip/v2/resource/light`,
       {
         headers: {
           "hue-application-key": username,
-        //   "Access-Control-Allow-Origin": "*",
-        //   "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
         },
       }
     );
