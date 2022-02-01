56994e465c8c52fb239556f15aeb037771050d7d
Xavier <xaviersavoie@gmail.com>
2022-02-01 14:41:08 -0500

    implemented get scene route

--- backend/routes/scene.js
+++ backend/routes/scene.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const scene = express.Router();
+const axios  = require('axios');
+require('dotenv').config();
 
-scene.get('/', function(req, res) {
-  res.send('test -- scene');
+const bridge = process.env.HUE_BRIDGE;
+const username = process.env.HUE_USERNAME;
+
+scene.get('/', async function(req, res) {
+  try {
+    const apiResponse = await axios.get(
+      `https://${bridge}/clip/v2/resource/scene`,
+      {
+        headers: {
+          "hue-application-key": username,
+        },
+      }
+    );
+    console.log(apiResponse.data.data)
+    res.json(apiResponse.data.data)
+  } catch (error) {
+    console.log(error);
+  }
+  // res.send('test -- scene');
 });
 
 module.exports = scene;
