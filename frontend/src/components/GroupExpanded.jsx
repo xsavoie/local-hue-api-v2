95f519b5612112db5fca5713750b3af28b4fee34
Xavier <xaviersavoie@gmail.com>
2022-02-10 18:51:41 -0500

    created groupexpanded component

---
+++ frontend/src/components/GroupExpanded.jsx
@@ -0,0 +1,21 @@
+import { useGroupState } from "../lib/groupState";
+import "./styles/GroupExpanded.css";
+
+export default function GroupExpanded({ groups, setGroups, scenes }) {
+  const { selected, setSelected } = useGroupState();
+
+  const selectedGroup = groups.filter((group) => group.id === selected);
+  const groupData = selectedGroup[0];
+
+  return (
+    <div className="group-expanded">
+      <header className="expanded--header">
+        <p>{groupData.name}</p>{" "}
+        <button type="button" onClick={() => setSelected("")}>
+          &times;
+        </button>
+      </header>
+      <div className="expanded--control"></div>
+    </div>
+  );
+}
