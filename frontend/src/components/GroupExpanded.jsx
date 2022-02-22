8a8d177f587e8e5dfa0cb908cf778a349e216a3b
Xavier <xaviersavoie@gmail.com>
2022-02-22 15:08:44 -0500

    setup light control logic in groupexpanded component

--- frontend/src/components/GroupExpanded.jsx
+++ frontend/src/components/GroupExpanded.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import useHueLight from "../hooks/useHueLight";
 import { useGroupState } from "../lib/groupState";
-import { getAllLightsForGroup, lightsIdForRoom } from "../lib/lightGroupHelpers";
+import {
+  getAllLightsForGroup,
+  lightsIdForRoom,
+} from "../lib/lightGroupHelpers";
 import { parseScenes } from "../lib/scenesHelper";
 import "../styles/GroupExpanded.css";
 
@@ -8,10 +13,19 @@ export default function GroupExpanded({ rooms, lights, setLights, scenes }) {
 
   const selectedRoom = rooms.filter((room) => room.id === selected);
   const roomData = selectedRoom[0];
-  
-  const groupLightId = lightsIdForRoom(roomData)
-  const groupLights = getAllLightsForGroup(groupLightId, lights)
-  const groupScenes = parseScenes(roomData, scenes)
+
+  const groupLightId = lightsIdForRoom(roomData);
+  const groupLights = getAllLightsForGroup(groupLightId, lights);
+  const groupScenes = parseScenes(roomData, scenes);
+
+  const { handleRequest, handleSceneChange } = useHueLight({
+    lights,
+    setLights,
+  });
+  const [bri, setBri] = useState(groupLights[0].dimming.brightness);
+  const [color, setColor] = useState(groupLights[0].color.xy);
+  const [selectedScene, setSelectedScene] = useState("");
+
 
   console.log(groupScenes);
 
@@ -23,7 +37,32 @@ export default function GroupExpanded({ rooms, lights, setLights, scenes }) {
           &times;
         </button>
       </header>
-      <div className="expanded--control"></div>
+      <div className="expanded--control">
+        {/* <ToggleLight
+          id={lightsId}
+          lightInfo={lightsInGroup[0]}
+          handleRequest={handleRequest}
+        /> */}
+        {/* <BrightnessSlider
+          id={lightsId}
+          bri={bri}
+          setBri={setBri}
+          handleRequest={handleRequest}
+        /> */}
+        {/* <ColorPicker
+          id={lightsId}
+          color={color}
+          bri={bri}
+          setColor={setColor}
+          handleRequest={handleRequest}
+        /> */}
+        {/* <ScenesControl
+          scenes={groupScenes}
+          handleSceneChange={handleSceneChange}
+          selectedScene={selectedScene}
+          setSelectedScene={setSelectedScene}
+        /> */}
+      </div>
     </div>
   );
 }
